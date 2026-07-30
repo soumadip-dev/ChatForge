bf92cba05fc9b1f616de7bfd0946b13be31bb44e
soumadip-dev <soumadipmajila@gmail.com>
2026-07-31 02:01:33 +0530

    feat: add custom AppError class and global error handler middleware

--- src/middlewares/errorhandler.middleware.ts
+++ src/middlewares/errorhandler.middleware.ts
@@ -1,10 +1,20 @@
 import type { Request, Response, NextFunction } from 'express';
 import { logger } from '../lib/logger.lib';
+import { AppError } from '../errors/AppError';
 
-export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
+export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction): void {
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+
+    return;
+  }
+
   logger.error({ err }, 'Unhandled error');
 
-  return res.status(500).json({
+  res.status(500).json({
     success: false,
     message: 'Internal server error',
   });
