ca3e66d28d6516bf4c3284a98e0e106843f297b5
soumadip-dev <soumadipmajila@gmail.com>
2026-09-13 22:50:54 +0530

    feat: add rate limiting to authenticated routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express';
 
 import { login, logout, register, profile, deleteAccount } from '../controllers/auth.controller';
-import { validate } from '../middlewares/validate.middleware';
-import { loginSchema, registerSchema } from '../validators/auth.validator';
+
 import { authenticate } from '../middlewares/auth.middleware';
+import { unauthenticatedRateLimiterMiddleware } from '../middlewares/unauth-rate-limiter.middleware';
+import { validate } from '../middlewares/validate.middleware';
+
+import { loginSchema, registerSchema } from '../validators/auth.validator';
+import { authenticatedRateLimiterMiddleware } from '../middlewares/auth-rate-limiter.middleware';
 
 export const authRouter = Router();
 
-authRouter.post('/register', validate(registerSchema), register);
-authRouter.post('/login', validate(loginSchema), login);
-authRouter.post('/logout', logout);
-authRouter.get('/profile', authenticate, profile);
-authRouter.delete('/delete', authenticate, deleteAccount);
+authRouter.post(
+  '/register',
+  unauthenticatedRateLimiterMiddleware,
+  validate(registerSchema),
+  register
+);
+authRouter.post('/login', unauthenticatedRateLimiterMiddleware, validate(loginSchema), login);
+authRouter.post('/logout', authenticate, authenticatedRateLimiterMiddleware, logout);
+authRouter.get('/profile', authenticate, authenticatedRateLimiterMiddleware, profile);
+authRouter.delete('/delete', authenticate, authenticatedRateLimiterMiddleware, deleteAccount);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,10 +7,11 @@ import { authRouter } from './auth.routes';
 import { chatRouter } from './chat.routes';
 import { authenticate } from '../middlewares/auth.middleware';
 import { messageRouter } from './message.routes';
+import { authenticatedRateLimiterMiddleware } from '../middlewares/auth-rate-limiter.middleware';
 
 export const apiRouter = Router();
 
 apiRouter.use(healthRouter);
 apiRouter.use('/auth', authRouter);
-apiRouter.use('/chat', authenticate, chatRouter);
-apiRouter.use('/message', authenticate, messageRouter);
+apiRouter.use('/chat', authenticate, authenticatedRateLimiterMiddleware, chatRouter);
+apiRouter.use('/message', authenticate, authenticatedRateLimiterMiddleware, messageRouter);
